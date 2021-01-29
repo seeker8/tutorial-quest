737f30aeac1fbfd20845d9a029d1eeb3064755dc
seeker <chuyo_gk@yahoo.com.mx>
2021-01-29 01:30:32 -0500

    set outside map as initial map

--- data/scripts/initial_game.lua
+++ data/scripts/initial_game.lua
@@ -14,7 +14,7 @@ function initial_game:initialize_new_savegame(game)
 
   -- You can modify this function to set the initial life and equipment
   -- and the starting location.
-  game:set_starting_location("first_map", "starting_destination")
+  game:set_starting_location("outside", "destination")
   -- game:set_starting_location("test_map/dungeon_entrance", "starting_destination")
 
   game:set_max_life(6)
